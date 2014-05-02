57afc83e2c00f7cafbf7578281ea6dc10dce294f
Farley Knight <farleyknight@gmail.com>
2014-05-02 10:10:51 -0400

    Bit refactoring. Included readline support.

--- javaRepl/run.sh
+++ javaRepl/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mvn exec:java -Dexec.mainClass="com.farleyknight.App"
+
+# mvn exec:java -Dexec.mainClass="com.farleyknight.App"
+java -jar target/javaRepl-1.0-SNAPSHOT-jar-with-dependencies.jar
